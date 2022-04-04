077f66ae5a0859719e44e58c66e8f34886faf6b2
omirege21 <89795807+omirege21@users.noreply.github.com>
2022-04-04 10:52:12 -0400

    Changes

--- js/video.js
+++ js/video.js
@@ -1,11 +1,17 @@
-var video;
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Playing Video")
+	video.play()
+	document.querySelector("#volume").innerHTML = video.volume * 100 +"%"
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pausing Video")
+	video.pause()
+});
